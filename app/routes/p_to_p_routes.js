e5021d9f1fbb1f3059ef15a02b0f11cd7a591738
Andrew McKinnon <andrewmckinnon2@gmail.com>
2018-10-27 14:04:30 -0400

    Added skeleton for requests

--- app/routes/p_to_p_routes.js
+++ app/routes/p_to_p_routes.js
@@ -2,4 +2,32 @@ module.exports = function(app, db) {
   app.get('/notes', (req, res) => {
      res.send('Hello');
    });
+
+   app.post('/user_creation', (req, res) => {
+
+   });
+
+   app.get('/user_info', (req, res) => {
+
+   });
+
+   app.put('/update_preferences', (req, res) => {
+
+   });
+
+   app.put('/make_match', (req, res) => {
+
+   });
+
+   app.get('/matches', (req, res) => {
+
+   });
+
+   app.get('/loans', (req, res) => {
+
+   });
+
+   app.post('/loan', (req, res) => {
+
+   });
 };
