6310c3d4d24c51559fb61137e2900f3729b5dd38
Andrew McKinnon <andrewmckinnon2@gmail.com>
2018-10-27 14:26:23 -0400

    Added mongodb to packages and url for use of the client to p_to_p_routes

--- app/routes/p_to_p_routes.js
+++ app/routes/p_to_p_routes.js
@@ -1,33 +1,36 @@
+var MongoClient = require('mongodb').MongoClient;
+var uri = 'mongodb+srv://andrewmckinnon2:Pass@4wed123!!!@lindrcluster-ifwaa.gcp.mongodb.net/test?retryWrites=true';
+
 module.exports = function(app, db) {
   app.get('/notes', (req, res) => {
      res.send('Hello');
    });
 
    app.post('/user_creation', (req, res) => {
-
+     res.send('Hello');
    });
 
    app.get('/user_info', (req, res) => {
-
+     res.send('Hello');
    });
 
    app.put('/update_preferences', (req, res) => {
-
+     res.send('Hello');
    });
 
    app.put('/make_match', (req, res) => {
-
+     res.send('Hello');
    });
 
    app.get('/matches', (req, res) => {
-
+     res.send('Hello');
    });
 
    app.get('/loans', (req, res) => {
-
+     res.send('Hello');
    });
 
    app.post('/loan', (req, res) => {
-
+     res.send('Hello');
    });
 };
